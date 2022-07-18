fed1cc503e91141d3f7111d47acaf0c30934efb2
bd2051 <bd2051@mail.ru>
2022-07-18 16:01:16 +0300

    fix

--- dev/model/Author.ts
+++ dev/model/Author.ts
@@ -2,6 +2,7 @@ import PrimaryKey from "../../src/fields/PrimaryKey";
 import StringField from "../../src/fields/StringField";
 import BaseModel from "../../src/model/BaseModel";
 import EntityManager from "../../src/EntityManager";
+import NumberField from "../../src/fields/NumberField";
 
 export default class Author extends BaseModel {
   id: PrimaryKey
@@ -11,6 +12,6 @@ export default class Author extends BaseModel {
     super(em);
     this.id = new PrimaryKey(em)
     this.name = new StringField(em)
-    this.age = new StringField(em)
+    this.age = new NumberField(em)
   }
 }
