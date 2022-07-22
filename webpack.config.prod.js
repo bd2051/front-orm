dd06746fe868af6e1010f7f6d8a75298a7474ded
bd2051 <bd2051@mail.ru>
2022-07-22 15:11:58 +0300

    dist

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,9 +25,6 @@ module.exports = {
           {
             loader: 'awesome-typescript-loader',
             options: {
-              query: {
-                declaration: false,
-              }
             },
           }
         ]
